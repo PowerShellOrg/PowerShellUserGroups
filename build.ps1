a308cc8554aa4f57f423e4fb1cc275ef78cb3215
Jeff Hicks <jhicks@jdhitsolutions.com>
2018-08-21 15:22:50 -0400

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,8 +1,11 @@
+#requires -version 5.0
+
 #run this command after new user groups have been added
 #to generate updated markdown and PDF documents.
 #this script requires the nodejs and the markdown-pdf package
 
 #for best results, run this in PowerShell Core on Windows
+# pwsh -file <path>\build.ps1
 
 .\New-MarkdownList.ps1
 .\New-UserGroupMarkdown.ps1
@@ -11,4 +14,4 @@ markdown-pdf -s .\markdown-pdf.css -f Letter .\PowerShellUserGroups.md
 get-childitem *.md,*.pdf -Exclude README.md
 get-childitem *.pdf | foreach-object {start $_.fullname}
 
-write-host "If PDFs look ok, commit these changes and push to GitHub." -ForegroundColor cyan
+write-host "`nIf PDFs look ok, commit these changes and push to GitHub." -ForegroundColor cyan
